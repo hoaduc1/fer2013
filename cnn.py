6519113b4debb5ea274e49609df2706db926eac2
Van Huong <vnnvanhuong@gmail.com>
2020-01-08 23:24:11 +0700

    apply l2 regularization, batch normalization

--- cnn.py
+++ cnn.py
@@ -1,11 +1,10 @@
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
-from keras.layers import Dropout
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
+from keras.layers import Flatten, Dense, BatchNormalization, Dropout
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.losses import mean_squared_error, categorical_crossentropy
 from keras.optimizers import SGD, Adam
 from keras.utils import to_categorical
+from keras.regularizers import l2
 import numpy as np
 
 from utils import load_data, plot_dataset, plot_training_history
@@ -35,19 +34,21 @@ x_train /= np.std(x_train, axis=0)
 
 # create model
 model = Sequential()
-model.add(Conv2D(30, (3, 3), input_shape=(48, 48, 1), activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(15, (3, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), input_shape=(48, 48, 1), activation='relu', data_format='channels_last', kernel_regularizer=l2(0.01)))
+model.add(BatchNormalization())
+model.add(Conv2D(16, (3, 3), activation='relu'))
 model.add(MaxPooling2D())
 model.add(Dropout(0.2))
-model.add(Flatten(input_shape=(48, 48)))
+
+model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dropout(0.4))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(7, activation='softmax'))
 
 model.compile(optimizer=Adam(lr=0.001), loss=mean_squared_error, metrics=['accuracy'])
-history = model.fit(x_train, y_train, batch_size=100, epochs=20, validation_data=(x_test, y_test))
+history = model.fit(x_train, y_train, batch_size=100, epochs=20, validation_data=(x_test, y_test), shuffle=True)
 
 plot_training_history(history)
 	
